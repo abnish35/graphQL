44dd632a36ae5153935fe8a1dd47bf41e320dc94
Abnish <akp12556@gmail.com>
2020-09-13 01:55:24 +0530

    filter, search operation, array-2

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -9,10 +9,23 @@ import { GraphQLServer } from 'graphql-yoga';
 
 // At the end of eact data type, there is "!" mark, that show that This type will return someting other than null. if you donot put "!" at the end of data type, it will give "null" value to it.
 
+// create a user data
+const users = [
+    { id: "1", name: "Abnish", email: "abnish@example.com", age: 26 },
+    { id: "2", name: "Alok", email: "alok@example.com" },
+    { id: "3", name: "Rahul", email: "rahul@example.com", age: 24 }
+]
+
+const posts =[
+    { id: "123", title: "GraphQL learning", body: "Best way to learn it", published: false },
+    { id: "157", title: "A way to learn", body: "Fantastic GraphQL Book Every one should read", published: true },
+    { id: "234", title: "A full stack developer", body: "Become a MERN Stack Developer", published: true }
+]
+
 const typeDefs = `
     type Query{
-        greetings(name: String, position: String): String!
-        addValue(v1: Float!, v2: Float!): String!
+        users(query: String): [User!]! 
+        posts(query: String): [Post]!
         me: User!
         post: Post!
     }
@@ -28,34 +41,33 @@ const typeDefs = `
         id: ID!
         title: String!
         body: String!
-        published: Int!
+        published: Boolean!
     }
 `
 const resolvers = {
     Query:{
 
-        addValue(parent, args, ctr, info){
-            if(args.v1 && args.v2){
-                return  `Sum is ${args.v1}` + `${args.v2}`
+        posts(parent, args, ctx, info){
+            if(!args.query){
+                return posts
             }
-            else if(args){
-                return args
+            return posts.filter(post=> {
+
+                const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+                const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+                
+                return isTitleMatch || isBodyMatch
+             })},
+
+        users(parent, args, ctx, info){
+            if(!args.query){
+                return users
             }
+
+            return users.filter(user=> user.name.toLocaleLowerCase().includes(args.query.toLowerCase())
+            )
         },
 
-        greetings(parent, args, ctr, info){
-            if(args.name && args.position){
-                return `Hello ${args.name}! I am a best ${args.position}`
-            }
-            else if(args.name){
-                return `Hello ${args.name}!`
-            }
-            else if(args.position){
-                return `Your position is ${args.position}`
-            }
-                return "Hello!"
-            
-        },
         me(){
             return{
                 id: "12345",
@@ -70,7 +82,7 @@ const resolvers = {
                 id: "45678",
                 title: "This is first Post",
                 body: "About GraphQL",
-                published: 2020
+                published: false
             }
         }
     }
@@ -86,6 +98,29 @@ server.start(()=>{
 })
 
 
+// IF you donot pass agruments and want to return an array, you can write code in this manner. 
+
+// type Query{
+//     users: [User!]! }
+
+// inside resolver 
+//   users(){
+//     return users
+// },
+
+// If you want to access these array data, write code in this manner in playground; 
+
+// query{
+	// users{
+    //     id
+    //     name
+    //     age
+    //     email
+    //   } }
+
+
+
+
 
 
 // const typeDefs =`
@@ -98,6 +133,60 @@ server.start(()=>{
       
 //     }
 // `
+
+// Array type 
+
+// type Query{
+//     greetings(name: String, position: String): String!
+//     addValue(v1: Float!, v2: Float!): String!
+//     addNumber(count: [Float!]!): Float!
+//     grades: [Int!]!
+//     me: User!
+//     post: Post!
+// }
+
+// Array Resolvers
+
+// addValue(parent, args, ctr, info){
+//     if(args.v1 && args.v2){
+//         return  `Sum is ${args.v1}` + `${args.v2}`
+//     }
+//     else if(args){
+//         return args
+//     }
+// },
+
+// // passed array as a arguments
+
+// addNumber(arparent, args, ctr, infogs){
+//     if(args.count.length === 0){
+//         return 0
+//     }
+
+//     return args.count.reduce((accumulator, currentValue)=>{
+//         return accumulator + currentValue
+//     })
+// },
+
+// // returm array
+// grades(){
+//     return [1,2,3]
+// },
+
+// greetings(parent, args, ctr, info){
+//     if(args.name && args.position){
+//         return `Hello ${args.name}! I am a best ${args.position}`
+//     }
+//     else if(args.name){
+//         return `Hello ${args.name}!`
+//     }
+//     else if(args.position){
+//         return `Your position is ${args.position}`
+//     }
+//         return "Hello!"
+    
+// },
+
 
 // const typeDefs = `
 //     type Query{
