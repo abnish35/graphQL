22d52f2177ff49516ff4fe0dbf33f83f2ce79775
Abnish <akp12556@gmail.com>
2020-09-12 22:26:34 +0530

    setup babel, custom data, schema and resolvers,

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,9 +1,106 @@
-import message, {  name, location, greetings } from './myModule'
-import differenceNumber,  { add } from './math';
+import { GraphQLServer } from 'graphql-yoga';
 
-console.log(name, location, message)
+// Type definition (schema)
+// Scalar Type or Data type in GraphQL 
+// String , Boolean, Int, Float, ID
 
-console.log(greetings("Banglore"))
+// Non Scalar Type
+// Object and Array
 
-console.log( "addition of number is: ", add(5,6))
-console.log('substraction of a number is :', differenceNumber(10, 8))
+// At the end of eact data type, there is "!" mark, that show that This type will return someting other than null. if you donot put "!" at the end of data type, it will give "null" value to it.
+
+const typeDefs = `
+    type Query{
+        me: User!
+        post: Post!
+    }
+
+    type User{
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    type Post{
+        id: ID!
+        title: String!
+        body: String!
+        published: Int!
+    }
+`
+const resolvers = {
+    Query:{
+        me(){
+            return{
+                id: "12345",
+                name: "Abnish",
+                email: "akp12556@gmail.com",
+                age: 24,
+                name: "Alok"
+            }
+        },
+        post(){
+            return{
+                id: "45678",
+                title: "This is first Post",
+                body: "About GraphQL",
+                published: 2020
+            }
+        }
+    }
+} 
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(()=>{
+    console.log(" The server is up!")
+})
+
+
+
+
+// const typeDefs =`
+//     type Query{
+//         id: ID!
+//         name: String!
+//         age: Int!
+//         employed: Boolean!
+//         gpa: Float!
+      
+//     }
+// `
+
+// const typeDefs = `
+//     type Query{
+//         title: String!
+//         price: Float!
+//         releasingYear: Int!
+//         rating: Float
+//         isStock: Boolean!
+//     }
+// `
+// // Resolvers
+
+// const resolvers = {
+//     Query: {
+//         id(){
+//             return "abc123"
+//         },
+//         name(){
+//             return "Abnish Kumar"
+//         },
+//         age(){
+//             return 27
+//         },
+//         employed(){
+//             return "Software Developer"
+//         },
+//         gpa(){
+//             return 3.1
+//         }
+//     }
+// }
--- graphql-basics/src/myModule.js
+++
@@ -1,9 +0,0 @@
-const name = "Abnish"
-const location = "Patna"
-const message = "Hi, Its time to code";
-
-const greetings =(location)=>{
-    return `welcome to ${location}`
-}
-
-export { name, location, message as default, greetings }
